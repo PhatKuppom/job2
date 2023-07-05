7ff9048dcfbba4db8b8486cc6fe57bd60deeb60a
PhatKuppom <phat777x2@gmail.com>
2023-07-05 11:35:07 +0700

    first commit

---
+++ member_insert.php
@@ -0,0 +1,13 @@
+<?php
+include "connect.php";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email, phone, photo, address)
+VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]','$_POST[phone]','$_POST[photo]','$_POST[address]')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
