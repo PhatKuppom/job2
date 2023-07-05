078c6b26fc4b678dcbf94753863bdb6e347f1ffc
PhatKuppom <phat777x2@gmail.com>
2023-07-05 14:47:25 +0700

    Update

---
+++ product_from.php
@@ -0,0 +1,8 @@
+<form action="product_insert.php" method="post" enctype="multipart/form-data">
+ชื่อ <input type="text" name="name"/>
+ราคา <input type="text" name="price"/> <br/>
+จำนวน <input type="text" name="stock"/> <br/>
+คำอธิบาย <input type="textarea" name="deacription"/> <br/>
+
+<input type="submit" value="บันทึกข้อมูล"/>
+</form>
